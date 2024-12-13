3b3d028b1661011c348827b6cabd9ecfa808fa9b
Damjan Pavlica <mudroljub@gmail.com>
2024-12-13 14:23:05 +0100

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -6,11 +6,10 @@ export default function About(): JSX.Element {
       <h2 className="text-xl">About</h2>
 
       <p className="mt-4">
-      Programming Quotes is a place where I’ve been collecting quotes and thoughts from the world of programming for years. These quotes are my way of reminding myself why I love coding.</p> 
+      <b>Programming Quotes</b> is a hobby project of mine that I have been developing for a long time. It started as a school project at the School of Code, but it is the only one that survived and continued to evolve.</p> 
       
-      <p className="mt-4">From deep wisdom to fun ones that remind us not to take ourselves too seriously – there’s something here for every programmer. Whether you’re a veteran or just starting out, I hope you enjoy the quotes and maybe find one that speaks to you.
+      <p className="mt-4">If you have any suggestions, feel free to contact me: <a href='mail&#116;o&#58;&#109;udro&#108;ju&#98;&#64;&#103;m%6&#49;&#37;69%6&#67;&#46;co&#109;'>mudrol&#106;ub&#64;g&#109;ail&#46;c&#111;m</a>
       </p>
-      <p className="mt-4"><Link href="https://github.com/mudroljub">mudroljub</Link></p>
     </div>
   );
 }
--- app/api/page.tsx
+++ app/api/page.tsx
@@ -5,9 +5,9 @@ export default function API(): JSX.Element {
     <div>
       <h2 className="text-xl">Programming Quotes API</h2>
       
-      <p className="mt-4">Github repo: <Link href="https://github.com/mudroljub/programming-quotes-api">github.com/mudroljub/programming-quotes-api</Link>
+      <p className="mt-4"><b>Github repo:</b> <Link href="https://github.com/mudroljub/programming-quotes-api">github.com/mudroljub/programming-quotes-api</Link>
       </p>
-      <p className="mt-4">Available at: <Link href="https://programming-quotes-api.azurewebsites.net/">programming-quotes-api.azurewebsites.net/</Link></p>
+      <p className="mt-4"><b>API:</b> <Link href="https://programming-quotes-api.azurewebsites.net/">programming-quotes-api.azurewebsites.net</Link></p>
     </div>
   );
 }
