1db4c93e29db0b96d7c06ab154bd62961954bb03
Damjan Pavlica <mudroljub@gmail.com>
2024-12-13 15:11:10 +0100

    icons

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -19,7 +19,7 @@ export default function Header() {
     <header className="bg-blue-500 text-white p-4">
       <div className="max-w-4xl mx-auto">
         {profile}
-        <h1 className="text-xl">Programming Quotes</h1>
+        <h1 className="text-xl">Programming Quotes 💬</h1>
         <nav>
           <ul className="flex space-x-4">
             <li>
