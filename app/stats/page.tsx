765f8bf1194309c7899506e92a905696056393c6
Damjan Pavlica <mudroljub@gmail.com>
2024-12-16 12:52:19 +0100

    useMemo

--- app/stats/page.tsx
+++ app/stats/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from "react";
+import React, { useMemo } from "react";
 import BarChart from '../components/BarChart'
 import PieChart from '../components/PieChart'
 import { useQuotes } from '../context/QuoteContext'
@@ -7,19 +7,20 @@ import { useQuotes } from '../context/QuoteContext'
 export default function About(): JSX.Element | null {
   const { quotes } = useQuotes()
 
-  const authorCount = quotes.reduce<Map<string, number>>((acc, quote) => {
-    acc.set(quote.author, (acc.get(quote.author) || 0) + 1)
-    return acc
-  }, new Map())        
-
-  const sorted = Array.from(authorCount.entries())
-    .sort(([keyA, valueA], [keyB, valueB]) => valueB - valueA)
+  const data = useMemo(() => {
+    const authorCount = quotes
+      .reduce<Map<string, number>>((dict, quote) => 
+         dict.set(quote.author, (dict.get(quote.author) || 0) + 1)
+      , new Map())
+    return Array.from(authorCount.entries())
+      .sort(([keyA, valueA], [keyB, valueB]) => valueB - valueA)
+  }, [quotes])
 
   return (
     <>
       <h2 className="text-xl mb-4">Quotes per author</h2>
-      <BarChart quoteCount={sorted} />
-      <PieChart quoteCount={sorted} />
+      <BarChart quoteCount={data} />
+      <PieChart quoteCount={data} />
     </>
-  );
+  )
 }
