1558a6c22c38f76446d524b1ecc1e288cc4cbcd9
Damjan Pavlica <mudroljub@gmail.com>
2024-12-17 09:51:43 +0100

    verifyEmail

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -3,12 +3,19 @@ import { useAuth } from '../context/AuthContext'
 import Privileges from '../components/Privileges'
 import { useQuotes } from '../context/QuoteContext'
 import BlockQuote from '../components/BlockQuote'
+import API from '../API'
 
 const Profile = () => {
   const { user, logout, loading } = useAuth()
   const { quotes, setQuotes } = useQuotes()
 
   const onDelete = (id: string) => setQuotes(quotes.filter(q => q._id !== id))
+
+  const verifyEmail = async() => {
+    console.log('verify')
+    const res = await API.POST('auth/email', {})
+    if (res.ok) alert('Email sent')
+  }
 
   if (loading) return <p>Loading...</p>
   if (!user) return <p>You are not logged in</p>
@@ -19,7 +26,12 @@ const Profile = () => {
     <div>
       <h2 className="text-xl mb-4">My Profile</h2>
 
-      <p className='mb-4'><strong>Email:</strong> {user.email}</p>
+      <p className='mb-2'><strong>Email:</strong> {user.email}</p>
+
+      {user.privilege === 0 &&
+        <button onClick={verifyEmail} className='rounded bg-transparent hover:bg-black text-black hover:text-white py-2 px-4 border border-black hover:border-transparent mb-4'>Verify ✉️</button>
+      }
+
       <p className='mb-2'><strong>Privilege:</strong></p>
       <Privileges privilege={user.privilege} className='mb-4' />
       <p className='mb-4'><strong>Member since:</strong> {new Date().toLocaleDateString()}</p>
